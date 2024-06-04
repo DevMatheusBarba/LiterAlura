e287e36b6112d63afe11161d4a1d3536f2678f4f
Matheus Silva <matheus.silva081@icloud.com>
2024-06-04 20:28:37 -0300

    Subindo o projeto

---
+++ src/main/java/br/com/alura/literalura/principal/Principal.java
@@ -0,0 +1,14 @@
+package br.com.alura.literalura.principal;
+
+import br.com.alura.literalura.service.ConsumoAPI;
+
+public class Principal {
+
+    private ConsumoAPI consumo = new ConsumoAPI();
+
+
+    public void exibeMenu() {
+
+        System.out.println("Hello World");
+    }
+}
